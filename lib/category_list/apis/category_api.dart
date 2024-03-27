56d02b4cfc7ec15629da36f2614f6c992fd0269c
Nicolas Cuillery <nicolas.cuillery@base1.com>
2024-03-27 11:01:53 -0400

    Move secrets in dotenv

--- lib/category_list/apis/category_api.dart
+++ lib/category_list/apis/category_api.dart
@@ -1,6 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:flex_storefront/category_list/models/category.dart';
-import 'package:flex_storefront/secrets.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 const DOMAIN = 'https://flex-cms-fpnplvnjqq-uc.a.run.app';
 const PATH = '/api/categories';
@@ -8,21 +8,16 @@ const PATH = '/api/categories';
 class CategoryApi {
   final http = Dio()
     ..options.headers = {
-      'Authorization': 'Bearer $strapiBearerToken',
+      'Authorization': 'Bearer ${dotenv.get('STRAPI_TOKEN')}',
     };
 
   Future<List<Category>> fetchCategories({int? parentId}) async {
     final response = await http.get(
         '$DOMAIN$PATH?filters[parent][id]${parentId == null ? '[\$null]=true' : '[\$eq]=$parentId'}&populate=*');
 
-    print(response.statusCode);
-    print(response.data['data']);
-
     final result = List<Category>.from(response.data['data'].map(
       (element) => Category.fromJson(element),
     ));
-
-    print(result);
 
     return result;
   }
