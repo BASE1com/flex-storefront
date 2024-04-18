ae79916b6c56bc703077bb0765d0b6387e8bd6e0
Stephen Richter <stephenrichter15@gmail.com>
2024-04-18 12:51:30 -0700

    feat(ui): adds widgetbook package

--- lib/flex_ui/widgets/add_to_cart_button.dart
+++ lib/flex_ui/widgets/add_to_cart_button.dart
@@ -1,15 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
 
 class AddToCartButton extends StatelessWidget {
-  const AddToCartButton({Key? key}) : super(key: key);
+  const AddToCartButton({
+    super.key,
+    this.title = 'Add to Cart',
+    this.onPressed,
+  });
+
+  final String title;
+  final VoidCallback? onPressed;
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: () {
-        // TODO: Add logic
-      },
+      onPressed: onPressed,
       child: const Text('Add to Cart'),
     );
   }
+}
+
+@widgetbook.UseCase(
+  name: 'Enabled',
+  type: AddToCartButton,
+)
+Widget enabledButton(BuildContext context) {
+  return Center(
+    child: AddToCartButton(
+      onPressed: () {},
+    ),
+  );
+}
+
+@widgetbook.UseCase(
+  name: 'Disabled',
+  type: AddToCartButton,
+)
+Widget disabledButton(BuildContext context) {
+  return const Center(
+    child: AddToCartButton(),
+  );
 }
