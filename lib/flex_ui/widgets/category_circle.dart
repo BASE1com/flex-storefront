8ddf72b865ca8a4130f0a149d0110616243a4603
Stephen Richter <stephenrichter15@gmail.com>
2024-04-19 12:05:02 -0700

    feat(ui): add CategoryCircle to Flex UI

---
+++ lib/flex_ui/widgets/category_circle.dart
@@ -0,0 +1,41 @@
+import 'package:flex_storefront/flex_ui/tokens/sizes.dart';
+import 'package:flex_storefront/flex_ui/widgets/cached_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class CategoryCircle extends StatelessWidget {
+  const CategoryCircle({
+    super.key,
+    required this.title,
+    required this.imageUrl,
+    this.onPressed,
+  });
+
+  final String title;
+  final String imageUrl;
+  final VoidCallback? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Column(
+        children: [
+          ClipOval(
+            child: CachedImage(url: imageUrl),
+          ),
+          const SizedBox(height: FlexSizes.spacerItems),
+          Text(
+            title,
+            style: const TextStyle(
+              fontSize: FlexSizes.fontSizeMd,
+              fontWeight: FontWeight.bold,
+              overflow: TextOverflow.ellipsis,
+            ),
+            maxLines: 2,
+          ),
+        ],
+      ),
+    );
+  }
+}
