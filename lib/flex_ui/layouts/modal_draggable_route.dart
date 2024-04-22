14faeb23ccfe5ba1cebaf92460e41a9354264fac
Stephen Richter <stephenrichter15@gmail.com>
2024-04-22 13:50:26 -0700

    feat(ui): add FlexModalSheet, FlexPill and nested json reader

---
+++ lib/flex_ui/layouts/modal_draggable_route.dart
@@ -0,0 +1,97 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flex_storefront/flex_ui/layouts/modal_draggable_sheet.dart';
+import 'package:flutter/material.dart';
+
+class ModalBottomSheetAutoRoute extends CustomRoute {
+  ModalBottomSheetAutoRoute({
+    required super.page,
+    super.path,
+    super.usesPathAsKey,
+    super.guards,
+    super.fullMatch,
+    super.meta,
+    super.maintainState,
+    super.fullscreenDialog,
+    super.children,
+    super.title,
+    super.restorationId,
+    super.keepHistory,
+    super.initial,
+    super.transitionsBuilder,
+    super.durationInMilliseconds,
+    super.reverseDurationInMilliseconds,
+    super.opaque,
+    super.barrierDismissible,
+    super.barrierLabel,
+    super.barrierColor,
+    double? fixedSize,
+    double initialSize = .5,
+    double minSize = .25,
+    double maxSize = .95,
+    ShapeBorder? shape,
+    bool? isScrollControlled,
+    bool? enableDrag,
+    bool? showDragHandle,
+    Color? backgroundColor,
+    bool? useSafeArea,
+    String? barrierOnTapHint,
+  }) : super(
+          customRouteBuilder: routeBuilderFactory(
+            initialChildSize: fixedSize ?? initialSize,
+            maxChildSize: fixedSize ?? maxSize,
+            minChildSize: fixedSize ?? minSize,
+            barrierColor: barrierColor,
+            barrierDismissible: barrierDismissible,
+            backgroundColor: backgroundColor,
+            barrierLabel: barrierLabel,
+            shape: shape,
+            isScrollControlled: isScrollControlled,
+            enableDrag: enableDrag,
+            showDragHandle: showDragHandle,
+            useSafeArea: useSafeArea,
+            barrierOnTapHint: barrierOnTapHint,
+          ),
+        );
+
+  static CustomRouteBuilder routeBuilderFactory({
+    required double initialChildSize,
+    required double minChildSize,
+    required double maxChildSize,
+    required bool barrierDismissible,
+    Color? backgroundColor,
+    Color? barrierColor,
+    ShapeBorder? shape,
+    bool? enableDrag,
+    bool? showDragHandle,
+    bool? isScrollControlled,
+    bool? useSafeArea,
+    String? barrierLabel,
+    String? barrierOnTapHint,
+  }) {
+    return <T>(BuildContext context, Widget child, AutoRoutePage<T> page) {
+      return ModalBottomSheetRoute<T>(
+        backgroundColor: backgroundColor,
+        isDismissible: barrierDismissible,
+        modalBarrierColor: barrierColor,
+        shape: shape,
+        isScrollControlled: isScrollControlled ?? true,
+        enableDrag: enableDrag ?? true,
+        showDragHandle: showDragHandle,
+        barrierLabel: barrierLabel,
+        settings: page,
+        useSafeArea: useSafeArea ?? false,
+        barrierOnTapHint: barrierOnTapHint,
+        builder: (context) {
+          return isScrollControlled ?? true
+              ? FlexModalDraggableSheet(
+                  initialChildSize: initialChildSize,
+                  minChildSize: minChildSize,
+                  maxChildSize: maxChildSize,
+                  child: child,
+                )
+              : child;
+        },
+      );
+    };
+  }
+}
---
+++ lib/shared/utils/nested_reader.dart
@@ -0,0 +1,29 @@
+import 'package:json_annotation/json_annotation.dart';
+
+Object? nestedReader(Map json, String key) {
+  final keys = key.split('.');
+  return _nestedReader(json, keys);
+}
+
+Object? _nestedReader(final Object? object, Iterable<String> keys) {
+  if (keys.isEmpty || object == null) {
+    return object;
+  }
+  if (object is Map) {
+    final subObject = object[keys.first];
+    final subKeys = keys.skip(1);
+    return _nestedReader(subObject, subKeys);
+  }
+  if (object is List) {
+    return object.fold<dynamic>([], (list, subObject) {
+      return list..add(_nestedReader(subObject, keys));
+    });
+  }
+  return object;
+}
+
+class NestedJsonKey extends JsonKey {
+  const NestedJsonKey({
+    required super.name,
+  }) : super(readValue: nestedReader);
+}
