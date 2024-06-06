f1531926bad3a20f7c275ba8ed9871c6a0467a89
Stephen Richter <stephenrichter15@gmail.com>
2024-06-05 21:17:20 -0700

    feat(35): lottie animation for place order success

--- lib/order/order_confirmation_page.dart
+++ lib/order/order_confirmation_page.dart
@@ -1,7 +1,11 @@
 import 'package:auto_route/annotations.dart';
+import 'package:auto_route/auto_route.dart';
 import 'package:flex_storefront/flex_ui/components/app_bar.dart';
+import 'package:flex_storefront/flex_ui/tokens/sizes.dart';
+import 'package:flex_storefront/shared/utils/image_strings.dart';
 import 'package:flutter/material.dart';
 import 'package:icons_plus/icons_plus.dart';
+import 'package:lottie/lottie.dart';
 
 @RoutePage()
 class OrderConfirmationPage extends StatelessWidget {
@@ -18,15 +22,41 @@ class OrderConfirmationView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: FlexAppBar(
+    return Scaffold(
+      appBar: const FlexAppBar(
         showSearchButton: false,
         showBackArrow: true,
         leadingIcon: LineAwesome.times_solid,
         title: Text('Order Confirmation'),
       ),
-      body: Center(
-        child: Text('Thank you for your order'),
+      body: Padding(
+        padding: const EdgeInsets.all(FlexSizes.appPadding),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Lottie.asset(
+              height: MediaQuery.of(context).size.height * 0.33,
+              FlexImages.animationOrderSuccess,
+              frameRate: FrameRate.max,
+            ),
+            Text(
+              'Order placed successfully!',
+              style: Theme.of(context).textTheme.headlineMedium,
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: FlexSizes.spacerItems),
+            Text(
+              'Thanks for your order, you will receive an email confirmation shortly.',
+              style: Theme.of(context).textTheme.bodyLarge,
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: FlexSizes.spacerSection),
+            ElevatedButton(
+              onPressed: () => context.router.maybePop(),
+              child: const Text('Continue Shopping'),
+            )
+          ],
+        ),
       ),
     );
   }
