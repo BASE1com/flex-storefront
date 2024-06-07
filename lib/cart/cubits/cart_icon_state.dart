4d5cd1f8a8806dc5411c87215d69141e374bc879
Stephen Richter <stephenrichter15@gmail.com>
2024-06-07 15:25:04 -0700

    fix(92): cart shows totalunitcount

--- lib/cart/cubits/cart_icon_state.dart
+++ lib/cart/cubits/cart_icon_state.dart
@@ -3,14 +3,29 @@ import 'package:flex_storefront/shared/bloc_helper.dart';
 class CartIconState {
   final Status status;
   final int totalItems;
+  final int totalUnitCount;
 
   CartIconState({
     required this.status,
     this.totalItems = 0,
+    this.totalUnitCount = 0,
   });
+
+  copyWith({
+    Status? status,
+    int? totalItems,
+    int? totalUnitCount,
+  }) {
+    return CartIconState(
+      status: status ?? this.status,
+      totalItems: totalItems ?? this.totalItems,
+      totalUnitCount: totalUnitCount ?? this.totalUnitCount,
+    );
+  }
 
   @override
   String toString() {
-    return 'CartIconState{status: $status, totalItems: $totalItems}';
+    return 'CartIconState{status: $status, totalItems: $totalItems, '
+        'totalUnitCount: $totalUnitCount}';
   }
 }
