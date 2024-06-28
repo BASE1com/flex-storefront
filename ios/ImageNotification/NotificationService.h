179c505d3b3b2b307eff483ec57e402ecee7eba1
Stephen Richter <stephenrichter15@gmail.com>
2024-06-28 13:56:15 -0700

    feat(push): upgrade Firebase and add ImageNotification target

---
+++ ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by Stephen Richter on 2024-06-28.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
